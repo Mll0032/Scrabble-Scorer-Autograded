b7a0cf66595a51b4824c0fb5d6a10208a2d3d0f1
mll0032 <mllawson18@gmail.com>
2024-07-09 19:33:25 -0500

    add newPointStructure object

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -32,6 +32,34 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+let newPointStructure = {
+  a:1,
+  b:3,
+  c:3,
+  d:2,
+  e:1,
+  f:4,
+  g:2,
+  h:4,
+  i:1,
+  j:8,
+  k:5,
+  l:1,
+  m:3,
+  n:1,
+  o:1,
+  p:3,
+  q:10,
+  r:1,
+  s:1,
+  t:1,
+  u:1,
+  v:4,
+  w:4,
+  x:8,
+  y:4,
+  z:10
+};
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
@@ -80,7 +108,7 @@ function vowelBonusScorer(word) {
   return `Score for ${word}': ${vBonusScore}`;
 }
 
-let newPointStructure;
+
 
 function scorerPrompt() {
   console.log("Which scoring algorithm would you like to use?");
@@ -102,6 +130,11 @@ function runProgram() {
    //let score = scorerVowelBonus(word);
    //let score = scorerSimple(word);
    //let score = oldScrabbleScorer(word);
+   //test newPointStructure object
+   //console.log("Scrabble scoring values for");
+   //console.log("letter a: ", newPointStructure.a);
+   //console.log("letter j: ", newPointStructure.j);
+   //console.log("letter z: ", newPointStructure["z"]);
 }
 
 // Don't write any code below this line //
